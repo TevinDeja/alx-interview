44ad1a26225e0ecc5b9a49216b2dd34ebed4daa6
TevinDeja <tevinowena@gmail.com>
2024-08-22 10:44:00 +0000

    NodeJS Basics

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,16 +13,16 @@ def makeChange(coins, total):
     Returns:
     int: Fewest number of coins needed to meet total, or -1 if not possible.
     """
+     if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        # Try each coin
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
