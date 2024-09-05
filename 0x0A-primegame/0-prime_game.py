5ef2f01edd23179eed083b419ab6fe27d56214a4
TevinDeja <tevinowena@gmail.com>
2024-09-05 07:02:06 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""Prime Game Module"""
-
-def is_prime(n):
-    """
-    Check if a number is prime
-    """
-    if n <= 1:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
 
 def isWinner(x, nums):
-    """
-    Determine the winner of the prime number game
-    """
-    if x == 0 or not nums:
+    def sieve_of_eratosthenes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                for j in range(i*i, n + 1, i):
+                    primes[j] = False
+        return primes
+
+    def play_game(n):
+        primes = sieve_of_eratosthenes(n)
+        moves = sum(primes)
+        return moves % 2 == 1  # Maria wins if odd number of moves
+
+    if not nums or x != len(nums):
         return None
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        primes = [i for i in range(1, n+1) if is_prime(i)]
-        while primes:
-            if len(primes) % 2 == 1:
-                maria_wins += 1
-            else:
-                ben_wins += 1
-            prime = primes[0]
-            primes = [i for i in primes if i % prime != 0]
+        if play_game(n):
+            maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
